25a081c0d950e1d44bda987e7201e4725b28c29c
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-09-11 07:04:20 -0300

    first commit

--- src/config/analytics.ts
+++
@@ -1,8 +0,0 @@
-// Google tracking
-// - Google Site Verification: for Google Search Console
-// - Google Analytics Measurement ID: for Google Analytics
-// - Google Tag Manager ID: for Google Tag Manager
-
-export const googleSiteVerification = ''
-export const googleAnalyticsMeasurementID = 'G-BMGF24DGTB'
-export const googleTagManagerID = 'G-BMGF24DGTB'
